d59cb8ff89caa4cf1488bbf097a6e75284e2e106
Ivo Peev <igpeev@gmail.com>
2019-03-29 01:41:38 +0200

    *args and **kwargs

---
+++ args_kwargs.py
@@ -0,0 +1,82 @@
+# *args and **kwargs (any name can be used i.e. *sun and **moon)
+# Good source: https://pythontips.com/2013/08/04/args-and-kwargs-in-python-explained/
+
+
+def test_1(arg1, arg2, arg3):
+    print(f'arg1: {arg1}')
+    print(f'arg2: {arg2}')
+    print(f'arg3: {arg3}')
+
+
+test_1('foo', 'bar', 'baz')
+# arg1: foo
+# arg2: bar
+# arg3: baz
+
+
+def test_2(*args):
+    # for arg in args:      # without 'idx'
+    for idx, arg in enumerate(args):
+        print(f'arg{idx}: {arg}')
+
+
+test_2('foo', 'bar', 'baz')
+# arg0: foo
+# arg1: bar
+# arg2: baz
+
+
+def test_3(**kwargs):
+    for k, v in kwargs.items():
+        print(f'{k}: {v}')
+    some_arg = kwargs.get('name') or 'No name found'
+    other_arg = kwargs.get('name1') or 'No name found'
+    third_arg = kwargs.get('funky')
+    print(f'some_arg: {some_arg}, other_arg: {other_arg}, third_arg: [{third_arg}]')
+
+
+test_3(name='Peter', age=33)
+# name: Peter
+# age: 33
+# some_arg: Peter, other_arg: No name found, third_arg: [None]
+
+
+# USING *args and **kwargs to call a function
+colors = {'red', 'green', 'blue'}
+data = ('two', 33, True)
+
+def do_sth(p1, p2, p3):
+    print(p1)
+    print(p2)
+    print(p3)
+
+do_sth(*colors)
+do_sth(*data)
+# blue
+# red
+# green
+# two
+# 33
+# True
+
+points = {'p1': 33, 'p2': 44, 'p3': 99}
+do_sth(**points)
+# 33
+# 44
+# 99
+
+
+# using all
+def some_calculation(parg, *args, **kwargs):
+    my_string = parg
+    my_tuple = args
+    my_dictionary = kwargs
+
+    print(my_string)
+    print(str(my_tuple))
+    print(str(my_dictionary))
+
+some_calculation('Peter', 33, 44, 99, name='Stephen', age=23)
+# Peter
+# (33, 44, 99)
+# {'name': 'Stephen', 'age': 23}
