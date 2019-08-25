2625e4dd599120d5f8b0f1fd8c842bbed3b6e7fc
Ivo Peev <igpeev@gmail.com>
2019-08-26 00:44:23 +0300

    args and kwargs extended

--- args_kwargs.py
+++ args_kwargs.py
@@ -14,19 +14,23 @@ test_1('foo', 'bar', 'baz')
 # arg3: baz
 
 
+
 def test_2(*args):
-    # for arg in args:      # without 'idx'
+    # for arg in args:                      # without 'idx'
+    print(type(args))                       # <-- TUPLE !!!
     for idx, arg in enumerate(args):
         print(f'arg{idx}: {arg}')
 
-
 test_2('foo', 'bar', 'baz')
+# <class 'tuple'>                           # <-- !!!
 # arg0: foo
 # arg1: bar
 # arg2: baz
 
 
+
 def test_3(**kwargs):
+    print(type(kwargs))                    # <-- DICTIONARY !!!
     for k, v in kwargs.items():
         print(f'{k}: {v}')
     some_arg = kwargs.get('name') or 'No name found'
@@ -34,11 +38,12 @@ def test_3(**kwargs):
     third_arg = kwargs.get('funky')
     print(f'some_arg: {some_arg}, other_arg: {other_arg}, third_arg: [{third_arg}]')
 
-
 test_3(name='Peter', age=33)
+# <class 'dict'>                           # <-- !!!
 # name: Peter
 # age: 33
 # some_arg: Peter, other_arg: No name found, third_arg: [None]
+
 
 
 # USING *args and **kwargs to call a function
@@ -80,3 +85,22 @@ some_calculation('Peter', 33, 44, 99, name='Stephen', age=23)
 # Peter
 # (33, 44, 99)
 # {'name': 'Stephen', 'age': 23}
+
+
+# BONUS: *args and *kwargs are convenient to forward to another func (to trace it)
+def trace(f, *args, **kwargs):
+    print(f'args: {args}')
+    print(f'kwargs: {kwargs}')
+    result = f(*args, **kwargs)     # <-- forward to other func and capture the result
+    print(f'result: {result}')
+    return result
+
+
+int('ff', base=16)                  # <-- converts base-16 int to decimal int
+# 255
+
+trace(int, 'ff', base=16)
+# args: ('ff',)
+# kwargs: {'base': 16}
+# result: 255
+# 255
